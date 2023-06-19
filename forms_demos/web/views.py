459c5a880645738bfa5ce41203dc3bc723347deb
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-06-19 21:31:59 +0300

    Initial commit

--- forms_demos/web/views.py
+++ forms_demos/web/views.py
@@ -2,5 +2,21 @@ from django.shortcuts import render
 from django import forms
 
 
+class NameForm(forms.Form):
+    your_name = forms.CharField(max_length=100)
+
+
 def index(request):
-    return render(request, 'web/index.html')
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        form.is_valid()
+        name = form.cleaned_data['your_name']
+    else:
+        form = NameForm()
+        name = None
+
+    context = {
+        'form': form,
+        'name': name,
+    }
+    return render(request, 'web/index.html', context)
