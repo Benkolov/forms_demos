b6f9e64710c3cf5e11a695aaa6af245b4b68cbda
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-06-19 22:49:11 +0300

    Initial commit

--- forms_demos/web/views.py
+++ forms_demos/web/views.py
@@ -1,18 +1,45 @@
+from django.forms import URLInput
 from django.shortcuts import render
 from django import forms
+from .models import Person
 
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(max_length=100)
+class PersonForm(forms.Form):
+    your_name = forms.CharField(
+        label="Your name",
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Your placeholder name',
+                'class': 'form-control',
+                'custom-attribute': 'custom-value'
+            }
+        ),
+    )
+
+    age = forms.IntegerField()
+
+    HOBBY_CHOICES = [
+        (1, 'Football'),
+        (2, 'Basketball'),
+        (3, 'Tennis'),
+    ]
+
+    hobby = forms.CharField(widget=forms.RadioSelect(choices=HOBBY_CHOICES))
+
+    is_happy = forms.BooleanField(required=False)
 
 
 def index(request):
     if request.method == 'POST':
-        form = NameForm(request.POST)
-        form.is_valid()
-        name = form.cleaned_data['your_name']
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+
+            Person.objects.create(name=form.cleaned_data['your_name'], age=form.cleaned_data['age'])
     else:
-        form = NameForm()
+        form = PersonForm()
         name = None
 
     context = {
